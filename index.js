9e86cfb61e11b752a77a0c99ea67b33755f3c4b6
G1ovann16 <glandaburo92@gmail.com>
2020-04-09 19:02:58 +0200

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000; 
+const cors = require('cors');
 
 const stadRouter = require('./routes/stad');
 const taskRouter = require('./routes/task');
@@ -8,6 +9,7 @@ const userRouter = require('./routes/user.js');
 const categoryRouter = require('./routes/category.js');
 
 
+app.use(cors());
 app.use(express.json()); //bodyparser
 
 
